ab2f6474c57dbc952f4589db03f0771a348d8b35
robin2017 <robin.seu@foxmail.com>
2020-08-17 23:49:20 +0800

    suc

--- app/controller/news.js
+++ app/controller/news.js
@@ -3,6 +3,7 @@ class NewsController extends Controller {
     async list() {
         //参数名 + 文件名 + 类的方法调用
         const data = await this.ctx.service.news.list()
+        this.logger.debug('list: %j', data);
         this.ctx.body = data
     }
 }
